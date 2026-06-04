9bef73ba2a00840ed976a675e68a12df08795c4e
Ahmed Dillo <142218167+Ahmeddillo@users.noreply.github.com>
2026-06-04 14:03:53 +0300

    Create ErrorViewModel.cs

---
+++ Arayuz/Models/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+// ErrorViewModel.cs
+namespace OtobusDashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
